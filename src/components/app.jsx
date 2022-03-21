f6669356a6196870bc113b3536f8c89861a0f958
tofusupreme <yunishbacon@gmail.com>
2022-03-21 21:55:24 +0900

    Basic React app-giphy search, icon not working

---
+++ src/components/app.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      selectedGifId: "diu1F4jUsuUzWewuKO"
+    };
+  }
+
+  search = (query) => {
+    // TODO: API call
+    giphy('AT7OW0hYZamRSLSuSjwxu9y8TmdLZ8uh').search({
+      q: query,
+    }, (err, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar search={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
