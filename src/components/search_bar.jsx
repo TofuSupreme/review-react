59e6dee6544a02b81bbc8b3423f3cca85da2fdab
tofusupreme <yunishbacon@gmail.com>
2022-05-06 16:50:29 +0900

    Updated search_bar.jsx

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ""
+    };
+  }
+
   handleUpdate = (e) => {
-    this.props.search(e.target.value);
+    this.props.searchFunction(e.target.value);
   }
 
   render() {
